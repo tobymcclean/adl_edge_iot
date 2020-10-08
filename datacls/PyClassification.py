be2f560d2303f2e8b1311b48dba3d5b1e68e0519
Toby McClean <toby.mcclean@adlinktech.com>
2020-10-08 11:12:49 -0400

    Update to the new data models and version 1.6.0 of Edge SDK

--- datacls/PyClassification.py
+++ datacls/PyClassification.py
@@ -2,70 +2,93 @@ from adlinktech.datariver import IotValue, IotNvp, IotNvpSeq
 
 
 class PyClassificationData:
-    def __init__(self, index: int = 0, output: str = '', label: str = '', probability: float = 0.0):
-        self.__index = IotValue()
-        self.__output = IotValue()
-        self.__label = IotValue()
-        self.__class_label = IotValue()
+    def __init__(self, category_id: int = -1, category_label='',
+                 super_category_id: int = -1, super_category_label='',
+                 metadata: str = '', probability: float = 0.0):
+        self.__category_id = IotValue()
+        self.__category_label = IotValue()
+        self.__super_category_id = IotValue()
+        self.__super_category_label = IotValue()
+        self.__metadata = IotValue()
         self.__probability = IotValue()
 
+        self.category_id = category_id
+        self.category_label = category_label
+        self.super_category_id = super_category_id
+        self.super_category_label = super_category_label
+        self.metadata = metadata
         self.probability = probability
-        self.index = index
-        self.output = output
-        self.label = label
 
     @property
-    def index(self) -> int:
-        return self.__index.int32
+    def category_id(self) -> int:
+        return self.__category_id.int64
 
-    @index.setter
-    def index(self, value: int) -> None:
-        self.__index.int32 = value
-
-    @property
-    def label(self) -> str:
-        return self.__label.string
-
-    @label.setter
-    def label(self, value: str) -> None:
-        self.__label.string = value if value is not None else ''
+    @category_id.setter
+    def category_id(self, value: int) -> None:
+        self.__index.int64 = value
 
     @property
-    def output(self) -> str:
-        return self.__output.string
+    def category_label(self) -> str:
+        return self.__category_label.string
 
-    @output.setter
-    def output(self, value: str) -> None:
-        self.__output.string = value
+    @category_label.setter
+    def category_label(self, value: str) -> None:
+        self.__category_label.string = value if value is not None else ''
+
+    @property
+    def super_category_id(self) -> int:
+        return self.__super_category_id.int64
+
+    @super_category_id.setter
+    def super_category_id(self, value: int) -> None:
+        self.__super_category_id.int64 = value
+
+    @property
+    def super_category_label(self) -> str:
+        return self.__super_category_label.string
+
+    @super_category_label.setter
+    def super_category_label(self, value: str) -> None:
+        self.__super_category_label.string = value
+
+    @property
+    def metadata(self) -> str:
+        return self.__metadata.string
+
+    @metadata.setter
+    def metadata(self, value: str) -> None:
+        self.__metadata.string = value
 
     @property
     def probability(self) -> float:
-        return self.__probability.float32
+        return self.__probability.float64
 
     @probability.setter
     def probability(self, value: float) -> None:
-        self.__probability.float32 = value
+        self.__probability.float64 = value
 
     @property
     def dr_data(self) -> IotNvpSeq:
         data = IotNvpSeq()
-        data.append(IotNvp('index', self.__index))
-        data.append(IotNvp('output', self.__output))
-        data.append(IotNvp('label', self.__label))
+        data.append(IotNvp('category_id', self.__category_id))
+        data.append(IotNvp('category_label', self.__category_label))
+        data.append(IotNvp('super_category_id', self.__super_category_id))
+        data.append(IotNvp('super_category_label', self.__super_category_label))
+        data.append(IotNvp('metadata', self.__metadata))
         data.append(IotNvp('probability', self.__probability))
         return data
 
 
 class PyClassification:
 
-    def __init__(self, engine_id: str = '', stream_id: str = '', frame_id: int = 0):
+    def __init__(self, engine_id: str = '', model_id: str = '', frame_id: int = 0):
         self.__engine_id = IotValue()
-        self.__stream_id = IotValue()
         self.__frame_id = IotValue()
+        self.__model_id = IotValue()
         self.__data = IotNvpSeq()
 
         self.engine_id = engine_id
-        self.stream_id = stream_id
+        self.model_id = model_id
         self.frame_id = frame_id
 
     @property
@@ -77,12 +100,12 @@ class PyClassification:
         self.__engine_id.string = value
 
     @property
-    def stream_id(self) -> str:
-        return self.__stream_id.string
+    def model_id(self) -> str:
+        return self.__model_id.string
 
-    @stream_id.setter
-    def stream_id(self, value: str) -> None:
-        self.__stream_id.string = value
+    @model_id.setter
+    def model_id(self, value: str) -> None:
+        self.__model_id.string = value
 
     @property
     def frame_id(self) -> int:
@@ -101,16 +124,20 @@ class PyClassification:
         self.__data.push_back(classification_data)
 
     def add_classification(self,
-                           index: int, output: str, label: str, probability: float) -> None:
-        self.add_data(PyClassificationData(index, output, label, probability))
+                           category_id: int = -1, category_label='',
+                           super_category_id: int = -1, super_category_label='',
+                           metadata: str = '', probability: float = 0.0) -> None:
+        self.add_data(
+            PyClassificationData(category_id, category_label, super_category_id, super_category_label, metadata,
+                                 probability))
 
     @property
     def dr_data(self) -> IotNvpSeq:
         data = IotNvpSeq()
         data.append(IotNvp('engine_id', self.__engine_id))
-        data.append(IotNvp('stream_id', self.__stream_id))
         data.append(IotNvp('frame_id', self.__frame_id))
-        dbox_value = IotValue()
-        dbox_value.nvp_seq = self.__data
-        data.append(IotNvp('data', dbox_value))
+        data.append(IotNvp('model_id', self.__model_id))
+        value = IotValue()
+        value.nvp_seq = self.__data
+        data.append(IotNvp('classification_data', value))
         return data
--- datacls/PyDetectionBox.py
+++ datacls/PyDetectionBox.py
@@ -1,134 +1,136 @@
 from adlinktech.datariver import IotValue, IotNvp, IotNvpSeq, IotByteSeq_from_buffer
 
+
 class PyDetectionBoxData:
-    def __init__(self, obj_id = 0, obj_label = '', class_id = 0, class_label = '', x1 = 0.0, y1 = 0.0, x2 = 0.0, y2 = 0.0, probability = 0.0, meta = ''):
-        self.__obj_id = IotValue()
-        self.__obj_label = IotValue()
-        self.__class_id = IotValue()
-        self.__class_label = IotValue()
+    def __init__(self, tracker_obj_id: int = 0, category_id: int = 0, category_label: str = '',
+                 x1: float = 0.0, y1: float = 0.0, x2: float = 0.0, y2: float = 0.0, probability: float = 0.0,
+                 color: str = '', metadata: str = ''):
+        self.__tracker_obj_id = IotValue()
+        self.__category_id = IotValue()
+        self.__category_label = IotValue()
         self.__x1 = IotValue()
         self.__y1 = IotValue()
         self.__x2 = IotValue()
         self.__y2 = IotValue()
         self.__probability = IotValue()
-        self.__meta = IotValue()
+        self.__color = IotValue()
+        self.__metadata = IotValue()
 
-        self.obj_id = obj_id
-        self.obj_label = obj_label
-        self.class_id = class_id
-        self.class_label = class_label
+        self.tracker_obj_id = tracker_obj_id
+        self.cateogry_id = category_id
+        self.category_label = category_label
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
         self.probability = probability
-        self.meta = meta
+        self.color = color
+        self.metadata = metadata
 
     @property
-    def obj_id(self):
-        return self.__obj_id.int32
+    def tracker_obj_id(self) -> int:
+        return self.__tracker_obj_id.int64
 
-    @obj_id.setter
-    def obj_id(self, value):
-        self.__obj_id.int32 = value
-
-    @property
-    def obj_label(self):
-        return self.__obj_label.string
-
-    @obj_label.setter
-    def obj_label(self, value):
-        self.__obj_label.string = value if value is not None else ''
+    @tracker_obj_id.setter
+    def tracker_obj_id(self, value: int) -> None:
+        self.__tracker_obj_id.int64 = value
 
     @property
-    def class_id(self):
-        return self.__class_id.int32
+    def category_id(self) -> int:
+        return self.__category_id.int64
 
-    @class_id.setter
-    def class_id(self, value):
+    @category_id.setter
+    def category_id(self, value: int) -> None:
         try:
-            self.__class_id.int32 = int(value)
+            self.__class_id.int64 = int(value)
         except TypeError:
             print(f'Classid type error {value}')
-            self.__class_id.int32 = -1
-
-
-    @property
-    def class_label(self):
-        return self.__class_label.string
-
-    @class_label.setter
-    def class_label(self, value):
-        self.__class_label.string = value if value is not None else ''
+            self.__class_id.int64 = -1
 
     @property
-    def x1(self):
-        return self.__x1.float32
+    def category_label(self) -> str:
+        return self.__category_label.string
+
+    @category_label.setter
+    def category_label(self, value: str) -> None:
+        self.__category_label.string = value if value is not None else ''
+
+    @property
+    def x1(self) -> float:
+        return self.__x1.float64
 
     @x1.setter
-    def x1(self, value):
-        self.__x1.float32 = value
+    def x1(self, value: float) -> float:
+        self.__x1.float64 = value
 
     @property
-    def y1(self):
-        return self.__y1.float32
+    def y1(self) -> float:
+        return self.__y1.float64
 
     @y1.setter
-    def y1(self, value):
-        self.__y1.float32 = value
+    def y1(self, value: float) -> None:
+        self.__y1.float64 = value
 
     @property
-    def x2(self):
-        return self.__x2.float32
+    def x2(self) -> float:
+        return self.__x2.float64
 
     @x2.setter
-    def x2(self, value):
-        self.__x2.float32 = value
+    def x2(self, value: float) -> None:
+        self.__x2.float64 = value
 
     @property
-    def y2(self):
-        return self.__y2.float32
+    def y2(self) -> float:
+        return self.__y2.float64
 
     @y2.setter
-    def y2(self, value):
-        self.__y2.float32 = value
+    def y2(self, value: float) -> None:
+        self.__y2.float64 = value
 
     @property
-    def probability(self):
-        return self.__probability.float32
+    def probability(self) -> float:
+        return self.__probability.float64
 
     @probability.setter
-    def probability(self, value):
-        self.__probability.float32 = value
+    def probability(self, value: float) -> None:
+        self.__probability.float64 = value
 
     @property
-    def meta(self):
-        return self.__meta.string
+    def color(self) -> str:
+        return self.__color.string
 
-    @meta.setter
-    def meta(self, value):
-        self.__meta.string = value if value is not None else ''
+    @color.setter
+    def color(self, value: str) -> None:
+        self.__color.string = value if value is not None else ''
+
+    @property
+    def metadata(self) -> str:
+        return self.__metadata.string
+
+    @metadata.setter
+    def metadata(self, value: str) -> None:
+        self.__metadata.string = value if value is not None else ''
 
     @property
     def dr_data(self):
         data = IotNvpSeq()
-        data.append(IotNvp('obj_id', self.__obj_id))
-        data.append(IotNvp('obj_label', self.__obj_label))
-        data.append(IotNvp('class_id', self.__class_id))
-        data.append(IotNvp('class_label', self.__class_label))
+        data.append(IotNvp('tracker_obj_id', self.__tracker_obj_id))
+        data.append(IotNvp('category_id', self.__category_id))
+        data.append(IotNvp('category_label', self.__category_label))
         data.append(IotNvp('x1', self.__x1))
         data.append(IotNvp('y1', self.__y1))
         data.append(IotNvp('x2', self.__x2))
         data.append(IotNvp('y2', self.__y2))
         data.append(IotNvp('probability', self.__probability))
-        data.append(IotNvp('meta', self.__meta))
+        data.append(IotNvp('color', self.__color))
+        data.append(IotNvp('metadata', self.__metadata))
         return data
 
 
 class PyDepthDetectionBoxData(PyDetectionBoxData):
     def __init__(self, obj_id=0, obj_label='', class_id=0, class_label='', x1=0.0, y1=0.0, x2=0.0, y2=0.0,
                  probability=0.0, meta='', dist_x=0.0, dist_y=0.0, dist_z=0.0):
-        super().__init__(obj_id, obj_label,class_id, class_label, x1, y1, x2, y2, probability, meta)
+        super().__init__(obj_id, obj_label, class_id, class_label, x1, y1, x2, y2, probability, meta)
         self.__dist_x = IotValue()
         self.__dist_y = IotValue()
         self.__dist_z = IotValue()
@@ -136,7 +138,6 @@ class PyDepthDetectionBoxData(PyDetectionBoxData):
         self.dist_x = dist_x
         self.dist_y = dist_y
         self.dist_z = dist_z
-
 
     @property
     def dist_x(self) -> float:
@@ -170,16 +171,15 @@ class PyDepthDetectionBoxData(PyDetectionBoxData):
         data.append(IotNvp('dist_z', self.__dist_z))
         return data
 
+
 class PyDetectionBox:
 
-    def __init__(self, engine_id = '', stream_id = '', frame_id=0):
+    def __init__(self, engine_id='', frame_id=0):
         self.__engine_id = IotValue()
-        self.__stream_id = IotValue()
         self.__frame_id = IotValue()
         self.__data = IotNvpSeq()
 
         self.engine_id = engine_id
-        self.stream_id = stream_id
         self.frame_id = frame_id
 
     @property
@@ -191,21 +191,12 @@ class PyDetectionBox:
         self.__engine_id.string = value
 
     @property
-    def stream_id(self):
-        return self.__stream_id.string
-
-    @stream_id.setter
-    def stream_id(self, value):
-        self.__stream_id.string = value
-
-    @property
     def frame_id(self):
         return self.__frame_id.uint32
 
     @frame_id.setter
     def frame_id(self, value):
         self.__frame_id.uint32 = value
-
 
     def add_data(self, value: PyDetectionBoxData):
         seq = value.dr_data
@@ -215,20 +206,21 @@ class PyDetectionBox:
         it.value = value
         self.__data.push_back(it)
 
-
-    def add_box(self,
-                obj_id, obj_label, class_id, class_label, x1, y1, x2, y2, probability, meta):
-        self.add_data(PyDetectionBoxData(obj_id, obj_label, class_id, class_label, x1, y1, x2, y2, probability, meta))
+    def add_box(self, tracker_obj_id: int = 0, category_id: int = 0, category_label: str = '',
+                x1: float = 0.0, y1: float = 0.0, x2: float = 0.0, y2: float = 0.0, probability: float = 0.0,
+                color: str = '', metadata: str = ''):
+        self.add_data(
+            PyDetectionBoxData(tracker_obj_id, category_id, category_label, x1, y1, x2, y2, probability, color,
+                               metadata))
 
     @property
     def dr_data(self) -> IotNvpSeq:
         data = IotNvpSeq()
         data.append(IotNvp('engine_id', self.__engine_id))
-        data.append(IotNvp('stream_id', self.__stream_id))
         data.append(IotNvp('frame_id', self.__frame_id))
         dbox_value = IotValue()
         dbox_value.nvp_seq = self.__data
-        data.append(IotNvp('data', dbox_value))
+        data.append(IotNvp('detection_box_data', dbox_value))
         return data
 
 
@@ -239,4 +231,3 @@ class PyDepthDetectionBox(PyDetectionBox):
                 dist_x, dist_y, dist_z):
         self.add_data(PyDepthDetectionBoxData(obj_id, obj_label, class_id, class_label, x1, y1, x2, y2,
                                               probability, meta, dist_x, dist_y, dist_z))
-
